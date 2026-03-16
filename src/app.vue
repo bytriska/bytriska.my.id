6365fb84114716a89a02834a7b97f4658cd84a7d
Triska Mahfud K <im.triskuy@gmail.com>
2026-03-16 14:27:47 +0000

    feat: delete unused design system preview page with all of the related components

--- src/app.vue
+++ src/app.vue
@@ -3,15 +3,14 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div class="min-h-dvh w-full flex flex-col items-center justify-center gap-4">
-    <main class="w-screen">
+  <div class="min-h-svh w-full flex flex-col gap-4">
+    <main class="w-full grow">
       <RouterView />
     </main>
-    <nav class="container-ds flex gap-4">
+    <nav class="container w-full flex justify-center gap-4 py-4">
       <RouterLink to="/"> Home </RouterLink>
       <RouterLink to="/posts"> Posts </RouterLink>
       <RouterLink to="/posts/example"> Example Post </RouterLink>
-      <RouterLink to="/design-system"> Design System </RouterLink>
     </nav>
   </div>
 </template>
