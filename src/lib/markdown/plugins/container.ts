a4984773c6e026cf7381cbfab4e5594d15300a21
Triska Mahfud K <im.triskuy@gmail.com>
2026-04-08 02:46:39 +0000

    fix: spacing style alert

--- src/lib/markdown/plugins/container.ts
+++ src/lib/markdown/plugins/container.ts
@@ -3,13 +3,16 @@ import type { MarkdownItAsync } from 'markdown-it-async'
 import { container as _containerPlugin } from '@mdit/plugin-container'
 
 export function containerPlugin(md: MarkdownItAsync) {
-  md.use(_containerPlugin, createAlertContainerOptions('tip'))
-  md.use(_containerPlugin, createAlertContainerOptions('info'))
-  md.use(_containerPlugin, createAlertContainerOptions('warning'))
-  md.use(_containerPlugin, createAlertContainerOptions('danger'))
+  md.use(_containerPlugin, createAlertContainerOptions('tip', md))
+  md.use(_containerPlugin, createAlertContainerOptions('info', md))
+  md.use(_containerPlugin, createAlertContainerOptions('warning', md))
+  md.use(_containerPlugin, createAlertContainerOptions('danger', md))
 }
 
-function createAlertContainerOptions(name: string): MarkdownItContainerOptions {
+function createAlertContainerOptions(
+  name: string,
+  md: MarkdownItAsync
+): MarkdownItContainerOptions {
   return {
     name,
     openRender: (tokens, idx, _opts, _env, self) => {
@@ -19,7 +22,7 @@ function createAlertContainerOptions(name: string): MarkdownItContainerOptions {
       const attrs = self.renderAttrs(token)
       const title = token.info.trim().slice(name.length).trim() || name.toUpperCase()
 
-      return `<div ${attrs}><div class="alert-title">${title}</div>`
+      return `<div ${attrs}><div class="alert-title">${md.renderInline(title)}</div>`
     },
   }
 }
