e92ac0535eb072f70173109a83b8ba32e8c98863
Triska Mahfud K <im.triskuy@gmail.com>
2026-05-11 15:40:48 +0000

    feat: update default layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,12 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { BaseMeta } from '@/types'
+
+const props = defineProps<Partial<BaseMeta>>()
+</script>
 
 <template>
   <div class="min-h-dvh w-full flex flex-col">
     <slot name="header">
-      <Header />
+      <Header class="[--ui-header-height:4rem]" />
     </slot>
 
-    <slot />
+    <main
+      class="grow w-full max-w-7xl mx-auto font-body text-base font-normal text-ui-content-base break-words overflow-x-hidden pt-8 px-6 pb-16 md:pt-12 md:px-8 md:pb-24 lg:pb-0"
+    >
+      <slot name="title">
+        <h1
+          v-if="props.title"
+          class="relative font-semibold text-ui-content-highlighted outline-none font-display tracking-tight text-3xl text-center text-pretty my-4"
+        >
+          {{ props.title }}
+        </h1>
+      </slot>
+      <slot name="description">
+        <p v-if="props.description" class="my-4 text-center text-pretty">
+          {{ props.description }}
+        </p>
+      </slot>
+      <hr class="my-4 border-0 border-t" />
+
+      <slot />
+    </main>
 
     <slot name="footer">
       <footer
--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
+import type { BaseMeta } from '@/types'
 import { usePosts } from '@/composables/posts'
+
+const meta: Partial<BaseMeta> = {
+  title: 'Posts',
+  description: 'A collection of all my posts.',
+}
 
 const { allPosts, allCategories, allTags, allSeries, featuredPosts } = usePosts()
 </script>
 
 <template>
-  <DefaultLayout>
+  <DefaultLayout v-bind="meta">
     <div
       class="grow w-full max-w-7xl flex flex-col items-center justify-center mx-auto px-4 sm:px-6 lg:px-8"
     >
