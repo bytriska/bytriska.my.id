48542da121f26e070821a494ee12de899c31cc3c
Triska <im.triskuy@gmail.com>
2026-09-14 03:35:48 +0000

    feat: add new theme

---
+++ src/pages/new-theme.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="new-theme size-full min-h-dvh flex flex-col">
+    <div class="grow size-full max-w-7xl mx-auto px-6 pt-8 pb-16 md:px-8 md:pt-12 md:pb-24 lg:pb-0">
+      <h1 class="text-3xl">hello</h1>
+    </div>
+  </div>
+</template>
